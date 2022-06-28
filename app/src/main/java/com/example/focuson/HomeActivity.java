7979150d588f3dfcaae6e761ae2ecc444bd8ffd9
Alifan12 <alif129310@gmail.com>
2022-06-29 00:18:33 +0700

    add halaman reward

--- app/src/main/java/com/example/focuson/HomeActivity.java
+++ app/src/main/java/com/example/focuson/HomeActivity.java
@@ -2,13 +2,28 @@ package com.example.focuson;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_home);
+    }
+
+    public void openListReward(View view) {
+        Intent intent = new Intent(this, RewardListActivity.class);
+        startActivity(intent);
+    }
+
+    public void openListTugas(View view) {
+        Intent intent = new Intent(this, TugasListActivity.class);
+        startActivity(intent);
     }
 }
