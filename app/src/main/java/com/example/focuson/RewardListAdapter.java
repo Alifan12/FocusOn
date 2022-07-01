4caf363017e2402c60a18fa4a567f74d01853f4b
Nabilkarren <nabilgunner@gmail.com>
2022-07-01 15:23:19 +0700

    Reward List Activity and XML

--- app/src/main/java/com/example/focuson/RewardListAdapter.java
+++ app/src/main/java/com/example/focuson/RewardListAdapter.java
@@ -1,7 +1,13 @@
 package com.example.focuson;
 
+import android.content.Context;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Adapter;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,29 +16,44 @@ import java.util.List;
 
 public class RewardListAdapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
 
-    List<String>
+    List<String>titles;
+    List<Integer>images;
+    Context context;
+    LayoutInflater inflater;
 
+    public Adapter(Context ctx, List<String> titles, List<Integer> images){
+        this.titles = titles;
+        this.images = images;
+        this.inflater = LayoutInflater.from(ctx);
+    }
 
     @NonNull
     @Override
-    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.activity_reward_list, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
-
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.title.setText(titles.get(position));
+        holder.gridIcon.setImageResource(images.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return titles.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder{
 
+        TextView title;
+        ImageView gridIcon;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            title = itemView.findViewById(R.id.tv_hadiah);
+            images = itemView.findViewById(R.id.iv_hadiah);
         }
     }
 
