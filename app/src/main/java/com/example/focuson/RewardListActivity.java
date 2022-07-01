31886b4dadb5ccd0e4013d38c1491b0d55cc8d09
Nabilkarren <nabilgunner@gmail.com>
2022-07-01 15:04:03 +0700

    Reward List Activity and XML

--- app/src/main/java/com/example/focuson/RewardListActivity.java
+++ app/src/main/java/com/example/focuson/RewardListActivity.java
@@ -1,4 +1,50 @@
 package com.example.focuson;
 
-public class RewardListActivity {
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RewardListActivity extends AppCompatActivity {
+    RecyclerView rewardList;
+    List<String> titles;
+    List<Integer> images;
+    RewardListAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        rewardList = findViewById(R.id.rv_rewardList);
+
+        titles = new ArrayList<>();
+        images = new ArrayList<>();
+
+        titles.add("Mainan");
+        titles.add("Sepatu");
+        titles.add("Permen");
+        titles.add("Baju");
+        titles.add("Lainnya+");
+
+        images.add(R.drawable.reward1);
+        images.add(R.drawable.reward2);
+        images.add(R.drawable.reward3);
+        images.add(R.drawable.reward4);
+        images.add(R.drawable.tambah_lainnya);
+
+        adapter = new RewardListAdapter(this, titles, images);
+
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 2, GridLayoutManager.VERTICAL, false);
+        rewardList.setLayoutManager(gridLayoutManager);
+        rewardList.setAdapter(adapter);
+    }
 }
--- app/src/main/java/com/example/focuson/RewardListAdapter.java
+++ app/src/main/java/com/example/focuson/RewardListAdapter.java
@@ -1,4 +1,40 @@
 package com.example.focuson;
 
-public class RewardListAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class RewardListAdapter extends RecyclerView.Adapter<Adapter.ViewHolder> {
+
+    List<String>
+
+
+    @NonNull
+    @Override
+    public Adapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Adapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
 }
+
